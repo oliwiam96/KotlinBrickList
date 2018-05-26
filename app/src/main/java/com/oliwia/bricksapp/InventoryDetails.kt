1d58f3c172f5f9233c920577b62e4dc15478d7f4
Oliwia <oliwia.m.96@wp.pl>
2018-05-26 14:29:54 +0200

    Create layout for details row

---
+++ app/src/main/java/com/oliwia/bricksapp/InventoryDetails.kt
@@ -0,0 +1,12 @@
+package com.oliwia.bricksapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class InventoryDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inventory_details)
+    }
+}
